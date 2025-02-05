65ba6094feba26cd6b48f871c7f23d53404b1cbe
sureshnewdevdev <45984902+sureshnewdevdev@users.noreply.github.com>
2025-02-05 08:26:56 +0530

    Add project files.

---
+++ Basics/Program.cs
@@ -0,0 +1,50 @@
+﻿using System; // #include stdio.h
+// high level program
+
+namespace Maths  // Collection of classes
+{
+    internal class Program // Collection of member vaiable and Member function
+    {
+        static void Main(string[] args) // function
+        {
+            #region Lession 1
+            //if (args.Length > 0)
+            //{
+            //    foreach (var item in args)
+            //    {
+            //        Console.WriteLine("Hello, " + item + "!");
+            //    }
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Hello, World!");
+            //    Console.WriteLine("Hello, World!");
+            //}
+            #endregion lession1
+
+
+            string strA = "";
+            string strB = "";
+
+            int a;
+            int b;
+            int c;
+
+            Console.WriteLine("Please enter value for A");
+            strA = Console.ReadLine();
+            Console.WriteLine("Please enter value for B");
+            strB = Console.ReadLine();
+
+            a = int.Parse(strA);
+            b = int.Parse(strB);
+
+            c = a + b;
+
+            Console.WriteLine("Sum of a and b is " + c);
+
+
+            Console.ReadLine();
+        }
+    }
+}
