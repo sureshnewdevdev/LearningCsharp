eab2917b9cca8cc8c2783c31a3a352307284efbc
sureshnewdevdev <45984902+sureshnewdevdev@users.noreply.github.com>
2025-02-11 08:36:10 +0530

    Basics -2

---
+++ Basics/Examples1.cs
@@ -0,0 +1,190 @@
+﻿//using System; // #include stdio.h
+//// high level program
+
+//namespace Maths  // Collection of classes
+//{
+//    internal class Program // Collection of member vaiable and Member function
+//    {
+//        static void Main(string[] args) // function
+//        {
+//            #region Lession 1
+//            //if (args.Length > 0)
+//            //{
+//            //    foreach (var item in args)
+//            //    {
+//            //        Console.WriteLine("Hello, " + item + "!");
+//            //    }
+
+//            //}
+//            //else
+//            //{
+//            //    Console.WriteLine("Hello, World!");
+//            //    Console.WriteLine("Hello, World!");
+//            //}
+//            #endregion lession1
+
+//            #region Lession 2
+
+//            //int? x = null;
+
+//            //string? strA = "";
+//            //string? strB = "";
+
+//            //int? a;
+//            //int? b;
+//            //int? c;
+
+//            //Console.WriteLine("Please enter value for A (max value is" + int.MaxValue +")" );
+//            //Console.Beep();
+
+//            //strA = Console.ReadLine();// "90" 90
+//            //Console.WriteLine("Please enter value for B (max value is" + int.MaxValue+") ");
+//            //Console.Beep();
+
+//            //strB = Console.ReadLine();
+
+
+
+
+//            //a = int.Parse(strA);
+//            //b = int.Parse(strB);
+
+//            //c = a + b;
+
+//            //Console.WriteLine("Sum of a and b is " + c);
+//            #endregion Lession 2
+
+//            #region Lession 4
+
+
+//            //// Float type
+
+//            //float? no1 = 10.57f;
+//            //float? no2 = 25.5f;
+//            //float? no3 = 0;
+
+//            //no3 = no1 + no2;
+
+//            //Console.WriteLine($"The result {no3}");
+
+
+
+//            //int mark1 = 100;
+//            //int mark2 = 100;
+//            //int mark3 = 100;
+//            //int mark4 = 100;
+//            //int mark5 = 100;
+
+//            //// Array means set of homogenious elements
+//            //int[] marks = new int[5];
+//            //marks[0] = 100;
+//            //marks[1] = 100;
+//            //marks[2] = 100;
+//            //marks[3] = 100;
+//            //marks[4] = 100;
+
+//            //int[] testArray = new int[int.MaxValue-1];
+
+//            //float[] weights = new float[5];
+//            //weights[0] = 90.5f;
+//            //weights[1] = 90.5f;
+//            //weights[2] = 90.5f;
+//            //weights[3] = 90.5f;
+//            //weights[4] = 90.5f;
+
+
+
+//            //String word = "Knowledge";
+//            //char[] charArray = word.ToCharArray();
+
+//            #endregion Lession 4
+
+//            #region Lession 5
+//            //byte byteValue = 0;/* (0 t0 255)*/
+
+//            ////byteValue = -10;
+
+//            //sbyte sb = -10;  // -128 to 127
+
+//            //short sh = -10; // -32768 to 32767
+
+//            //int i = -10; // -2,147,483,648 to 2,147,483,647
+//            //uint ui = 10; // 0 to 4,294,967,295
+
+//            //long l = -10; // -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+//            //ulong ul = 10; // 0 to 18,446,744,073,709,551,615
+
+//            //// *****************************************
+//            //// Floating - Point Types
+//            //// *****************************************
+
+//            //float f = 10.5f; // 7 digits 4 byte
+//            //double d = 10.5; // 15-16 digits 8 byte
+//            //decimal dec = 10.5m; // 28-29 digits 16 byte
+
+//            //bool isActive = true;
+//            //bool isClosed = false;
+
+//            //char ch = 'A'; // 2 byte
+//            #endregion
+
+//            #region Lession 6
+//            //Console.WriteLine("Enter the value for Num1");
+//            //int num1 = int.Parse(Console.ReadLine());
+
+//            //Console.WriteLine("Enter the value for Num2");
+//            //int num2 = int.Parse(Console.ReadLine()); ;
+
+//            //if(num1 > num2)
+//            //{
+//            //    Console.WriteLine("Num1 is greater than Num2");
+//            //}
+//            //else
+//            //{
+//            //    Console.WriteLine("Num2 is greater than Num1");
+//            //}
+
+//            ////int big =  num1 > num2 ? Console.WriteLine("Num1 is greater than Num2") : Console.WriteLine("Num2 is greater than Num1"); 
+//            //if (num1 > num2)
+//            //{
+//            //    Console.WriteLine("Num1 is greater than Num2");
+//            //}
+//            //else
+//            //{
+//            //    Console.WriteLine("Num2 is greater than Num1");
+//            //}
+//            #endregion
+
+//            // Write a program to check the bigger number and print the number
+
+//            //Console.WriteLine("Enter the value for Num1");
+//            //int num1 = int.Parse(Console.ReadLine());
+
+//            //Console.WriteLine("Enter the value for Num2");
+//            //int num2 = int.Parse(Console.ReadLine()); ;
+//            //int big = FindBig(num1, num2);
+//            //Console.WriteLine("The biggest value is  ");
+//            //Console.WriteLine(big);
+
+//            //if (num1 > num2)
+//            //{
+//            //    Console.WriteLine(num1);
+//            //}
+//            //else
+//            //{
+//            //    Console.WriteLine(num2);
+//            //}
+
+
+//            Console.ReadKey();
+
+
+
+//        }
+
+//        //private static int FindBig(int num1, int num2)
+//        //{
+//        //    return num1 > num2 ? num1 : num2;
+//        //}
+//    }
+//}
--- Basics/Program.cs
+++ Basics/Program.cs
@@ -1,50 +1,137 @@
-﻿using System; // #include stdio.h
-// high level program
+﻿using Microsoft.VisualBasic;
 
 namespace Maths  // Collection of classes
 {
     internal class Program // Collection of member vaiable and Member function
     {
-        static void Main(string[] args) // function
+        //static void Main(string[] args) // function
+        //{
+        //    Console.WriteLine("Enter the value for A");
+        //    int a = int.Parse(Console.ReadLine());
+        //    Console.WriteLine("Enter the value for B");
+        //    int b = int.Parse(Console.ReadLine());
+        //    Console.WriteLine("Enter the value for C");
+        //    int c = int.Parse(Console.ReadLine());
+
+        //    #region Nested if statement
+        //    //Nested if statement
+        //    //if (a > b)
+        //    //{
+        //    //    if (a > c)
+        //    //    {
+        //    //        Console.WriteLine("A is greater");
+        //    //    }
+        //    //    else
+        //    //    {
+        //    //        Console.WriteLine("C is greater");
+        //    //    }
+        //    //}
+        //    //else
+        //    //{
+        //    //    if (b > c)
+        //    //    {
+        //    //        Console.WriteLine("B is greater");
+        //    //    }
+        //    //    else
+        //    //    {
+        //    //        Console.WriteLine("C is greater");
+        //    //    }
+        //    //}
+        //    #endregion
+
+        //    #region if else if statement
+        //    if (a > b && a > c) // AND Condition
+        //    {
+        //        Console.WriteLine("A is greater");
+        //    }
+        //    else if (b > c)
+        //    {
+        //        Console.WriteLine("B is greater");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("C is greater");
+        //    }
+        //    #endregion
+
+
+        //}
+
+        #region Example for OR
+        //static void Main()
+        //{
+        //    int age = 20;
+        //    string country = "USA";
+
+        //    if (age >= 18 || country == "India")
+        //    {
+        //        Console.WriteLine("Eligible for the program.");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Not eligible for the program.");
+        //    }
+        //}
+        #endregion
+
+        #region Example for NOT
+        //static void Main()
+        //{
+        //    int n = 0;
+        //    if (n != 0)
+        //    {
+        //        Console.WriteLine("The number is not zero.");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("The number is zero.");
+        //    }
+
+        //    if (!(n == 0))
+        //    {
+        //        Console.WriteLine("The number is not zero.");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("The number is zero.");
+        //    }
+
+        //    n = 0;
+        //    bool result = !(n == 0);
+
+
+        //}
+        #endregion
+
+        #region If else if statement
+        static void Main()
         {
-            #region Lession 1
-            //if (args.Length > 0)
-            //{
-            //    foreach (var item in args)
-            //    {
-            //        Console.WriteLine("Hello, " + item + "!");
-            //    }
+            string fruitName = string.Empty;
 
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Hello, World!");
-            //    Console.WriteLine("Hello, World!");
-            //}
-            #endregion lession1
+            Console.WriteLine("Enter the fruit name");
+            fruitName = Console.ReadLine();
 
-
-            string strA = "";
-            string strB = "";
-
-            int a;
-            int b;
-            int c;
-
-            Console.WriteLine("Please enter value for A");
-            strA = Console.ReadLine();
-            Console.WriteLine("Please enter value for B");
-            strB = Console.ReadLine();
-
-            a = int.Parse(strA);
-            b = int.Parse(strB);
-
-            c = a + b;
-
-            Console.WriteLine("Sum of a and b is " + c);
-
-
-            Console.ReadLine();
+            if (fruitName == "Apple")
+            {
+                Console.WriteLine("The fruit is Apple");
+            }
+            else if (fruitName == "Banana")
+            {
+                Console.WriteLine("The fruit is Banana");
+            }
+            else if (fruitName == "Mango")
+            {
+                Console.WriteLine("The fruit is Mango");
+            }
+            else
+            {
+                Console.WriteLine("The fruit is not available");
+            }
+            #endregion
         }
     }
 }
+
+
+
+
