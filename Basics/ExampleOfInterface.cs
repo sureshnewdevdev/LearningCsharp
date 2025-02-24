5243606b1256f645e98898cc87a34881a5eb7d4a
sureshnewdevdev <45984902+sureshnewdevdev@users.noreply.github.com>
2025-02-24 07:58:50 +0530

    Interface example

---
+++ Basics/ExampleOfInterface.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    public interface IVehicle
+    {
+        string Name { get; set; }
+        int Speed { get; set; }
+        void Move();
+        void DriverQualification();
+    }
+
+    public interface IDrivingLicense
+    {
+        void ShowDrivingLicense();
+    }
+
+
+    public class SumoCar : IVehicle, IDrivingLicense
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public void Move()
+        {
+            Console.WriteLine($"{Name} moves on roads.");
+        }
+        public void DriverQualification()
+        {
+            Console.WriteLine("Road ways licenses");
+        }
+
+        public void ShowDrivingLicense()
+        {
+            Console.WriteLine("License Shown");
+        }
+    }
+
+    public class SpeedBoat : IVehicle
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public void Move()
+        {
+            Console.WriteLine($"{Name} moves on water.");
+        }
+        public void DriverQualification()
+        {
+            Console.WriteLine("Water ways licenses");
+        }
+
+    }
+
+    public class SpeedJet : IVehicle
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public void Move()
+        {
+            Console.WriteLine($"{Name} moves in air.");
+        }
+        public void DriverQualification()
+        {
+            Console.WriteLine("Air ways licenses");
+        }
+    }
+
+
+
+    public class ExampleOfInterface
+    {
+        public static void Main()
+        {
+            SumoCar car = new SumoCar { Name = "Tesla", Speed = 150 };
+            car.Move();
+            car.DriverQualification();
+            car.ShowDrivingLicense();
+            SpeedBoat boat = new SpeedBoat { Name = "Yacht", Speed = 50 };
+            boat.Move();
+            boat.DriverQualification();
+            SpeedJet jet = new SpeedJet { Name = "F-16", Speed = 1000 };
+            jet.Move();
+            jet.DriverQualification();
+ 
+            DisplayInfo(car);
+            DisplayInfo(boat);
+            DisplayInfo(jet);
+        }
+        public static string DisplayInfo(IVehicle vehicle)
+        {
+            if ( vehicle is IDrivingLicense )
+            {
+                IDrivingLicense drivingLicense = vehicle as IDrivingLicense;
+                drivingLicense.ShowDrivingLicense();
+            }
+            return $"Vehicle Name: {vehicle.Name}, Speed: {vehicle.Speed} km/h";
+        }
+
+    }
+}
+ 
---
+++ Basics/FunnyInterfaceExample.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    public interface IPeron
+    {
+        string Name { get; set; }
+        int Age { get; set; }
+        void DisplayInfo();
+    }
+
+
+    public class SMan : IPeron
+    {
+        public string Name { get ; set ; }
+        public int Age { get ; set ; }
+
+        public string FreeTravelMode()
+        {
+            return "Walking";
+        }
+        public void DisplayInfo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class SWoman : IPeron
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public string FreeTravelMode()
+        {
+            return "Bus";
+        }
+        public void DisplayInfo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class FunnyInterfaceExample
+    {
+
+    }
+}
