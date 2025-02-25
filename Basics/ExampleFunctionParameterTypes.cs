b382047a14c3b7841fa08c6d4cffa5e44ffb0142
sureshnewdevdev <45984902+sureshnewdevdev@users.noreply.github.com>
2025-02-25 08:43:00 +0530

    poly changes

---
+++ Basics/ExampleFunctionParameterTypes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    public class ExampleFunctionParameterTypes
+    {
+
+        public int Add(ref int a, int b)
+        {
+            a = 10;
+            return a + b;
+        }
+
+        public int Add(ref int a, int b, out int c)
+        {
+            a = 10;
+            c = 200;
+            return a + b;
+        }
+        public static void Main()
+        {
+            int a = 5;
+            int b = 10;
+            int c;
+           
+            ExampleFunctionParameterTypes obj = new ExampleFunctionParameterTypes();
+            int result = obj.Add(ref a, b);
+
+
+            result = obj.Add(ref a, b, out c);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+       
+       
+    }
+}
