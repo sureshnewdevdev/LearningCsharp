a710551aea50a75a28f907ae3febf0beae9b08a8
sureshnewdevdev <45984902+sureshnewdevdev@users.noreply.github.com>
2025-03-03 08:40:17 +0530

    Collection NOtes Added

--- Basics/CollectionExamples.cs
+++ Basics/CollectionExamples.cs
@@ -11,7 +11,133 @@ namespace Basics
     {
         static void Main(string[] args)
         {
-            ArrayList arrayList= new ArrayList();
+            ArrListExample();
+            Console.WriteLine("**********************************************************************");
+            StackExample();
+
+            HashTableExample();
+            QueueExample();
+            ExampleOfSortedList();
+            Console.ReadLine();
+        }
+
+        private static void ExampleOfSortedList()
+        {
+            // Creating a SortedList
+            SortedList sortedList = new SortedList();
+
+            // Adding key-value pairs
+            sortedList.Add(3, "Three");
+            sortedList.Add(1, "One");
+            sortedList.Add(2, "Two");
+
+            // Display elements (sorted by key)
+            Console.WriteLine("SortedList Elements:");
+            foreach (DictionaryEntry entry in sortedList)
+            {
+                Console.WriteLine($"{entry.Key}: {entry.Value}");
+            }
+
+            // Removing an element
+            sortedList.Remove(2);
+            Console.WriteLine("\nAfter Removing key 2:");
+            foreach (DictionaryEntry entry in sortedList)
+            {
+                Console.WriteLine($"{entry.Key}: {entry.Value}");
+            }
+        }
+
+        private static void QueueExample()
+        {
+            // Creating a Queue
+            Queue queue = new Queue();
+
+            // Enqueueing elements
+            queue.Enqueue("First");
+            queue.Enqueue("Second");
+            queue.Enqueue("Third");
+
+            // Dequeueing (removes front)
+            Console.WriteLine("Dequeued: " + queue.Dequeue());
+
+            // Peek (view front element)
+            Console.WriteLine("Front Element: " + queue.Peek());
+
+            // Display remaining elements
+            Console.WriteLine("\nQueue Elements:");
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static void HashTableExample()
+        {
+            // Creating a Hashtable
+            Hashtable ht = new Hashtable();
+
+            // Adding key-value pairs
+            ht.Add(1, "One");
+            ht.Add(2, "Two");
+            ht.Add(3, "Three");
+            ht.Add("Raja", "Rani");
+
+            Person p = new Person { PersonId= 1, PersonName="Tulsi" };
+
+            Stack stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            ht.Add("Stack Sub Collection", stack);
+
+            // Can Store custom Objects
+            ht.Add( "Person Object",p);
+
+
+            // Printing the value of key "Raja"
+            Console.WriteLine(ht["Raja"]);
+
+            // Displaying Hashtable
+            Console.WriteLine("Hashtable Elements:");
+            foreach (DictionaryEntry entry in ht)
+            {
+                Console.WriteLine($"{entry.Key}: {entry.Value}");
+            }
+
+            // Accessing value using key
+            Console.WriteLine($"\nValue for key 2: {ht[2]}");
+
+            // Removing an element
+            ht.Remove(1);
+            Console.WriteLine("\nAfter Removing key 1:");
+            foreach (DictionaryEntry entry in ht)
+            {
+                Console.WriteLine($"{entry.Key}: {entry.Value}");
+            }
+        }
+
+        private static void StackExample()
+        {
+            Stack stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            stack.Push(4);
+
+            int len = stack.Count;
+            for (int i = 0; i < len; i++)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+        }
+
+        private static void ArrListExample()
+        {
+            ArrayList arrayList = new ArrayList();
             arrayList.Add(1);
             arrayList.Add("Raja");
             arrayList.Add(1.2);
@@ -26,21 +152,6 @@ namespace Basics
             {
                 Console.WriteLine(arrayList[i]);
             }
-            Console.WriteLine("**********************************************************************");
-            Stack stack = new Stack();
-            stack.Push(1);
-            stack.Push(2);
-            stack.Push(3);
-                
-            stack.Push(4);
-
-            int len = stack.Count;
-            for (int i = 0; i < len; i++)
-            {
-                Console.WriteLine(stack.Pop());
-            }
-
-            Console.ReadLine();
         }
     }
 }
